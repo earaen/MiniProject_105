c3fa58bdac37d05ada307b06cb0a07afb0f40ae0
earaen <pavikaforlearning@gmail.com>
2023-06-02 15:57:13 +0700

    helloo

--- carepaw/frontend/src/components/ProfileCard/ProfileCard.jsx
+++ carepaw/frontend/src/components/ProfileCard/ProfileCard.jsx
@@ -35,7 +35,7 @@ function ProfileCard() {
     localStorage.removeItem("token");
 
     // Redirect to "/login" page
-    window.location.href = "/login";
+    window.location.href = "/";
   };
 
   const handleUpdateUser = async () => {
