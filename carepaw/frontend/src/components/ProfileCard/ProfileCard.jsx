81d8973c1affccfac1a7c9b0ac1d18e890f04e8f
earaen <pavikaforlearning@gmail.com>
2023-06-02 19:12:07 +0700

    done

--- carepaw/frontend/src/components/ProfileCard/ProfileCard.jsx
+++ carepaw/frontend/src/components/ProfileCard/ProfileCard.jsx
@@ -4,7 +4,8 @@ import "./ProfileCard.css";
 
 function ProfileCard() {
   const [user, setUser] = useState(null);
-  const [showModal, setShowModal] = useState(false);
+  const [showUpdateModal, setShowUpdateModal] = useState(false);
+  const [showLogoutModal, setShowLogoutModal] = useState(false);
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
 
@@ -29,15 +30,6 @@ function ProfileCard() {
     fetchUserInformation();
   }, []);
 
-  const handleLogout = () => {
-    // Clear local storage
-    localStorage.removeItem("userId");
-    localStorage.removeItem("token");
-
-    // Redirect to "/login" page
-    window.location.href = "/login";
-  };
-
   const handleUpdateUser = async () => {
     const userId = localStorage.getItem("userId");
     try {
@@ -51,7 +43,7 @@ function ProfileCard() {
       const data = await response.json();
       if (response.ok) {
         setUser(data);
-        setShowModal(false);
+        setShowUpdateModal(false);
       } else {
         console.error(data.error);
       }
@@ -60,8 +52,25 @@ function ProfileCard() {
     }
   };
 
-  const toggleModal = () => {
-    setShowModal(!showModal);
+  const handleLogout = () => {
+    setShowLogoutModal(true);
+  };
+
+  const confirmLogout = () => {
+    // Clear local storage
+    localStorage.removeItem("userId");
+    localStorage.removeItem("token");
+
+    // Redirect to "/login" page
+    window.location.href = "/login";
+  };
+
+  const cancelLogout = () => {
+    setShowLogoutModal(false);
+  };
+
+  const toggleUpdateModal = () => {
+    setShowUpdateModal(!showUpdateModal);
   };
 
   return (
@@ -84,7 +93,7 @@ function ProfileCard() {
           )}
 
           <div style={{ display: "flex", marginTop: "2rem" }}>
-            <button className="update" onClick={toggleModal}>
+            <button className="update" onClick={toggleUpdateModal}>
               Update
             </button>
             <button className="logout" onClick={handleLogout}>
@@ -94,7 +103,7 @@ function ProfileCard() {
         </div>
       </div>
 
-      {showModal && (
+      {showUpdateModal && (
         <div className="modal">
           <div className="modal-content">
             <label style={SECTION}>Username: </label>
@@ -131,11 +140,48 @@ function ProfileCard() {
                 marginTop: "1rem",
               }}
             >
-              <button className="CLL" onClick={toggleModal}>
+              <button className="CLL" onClick={toggleUpdateModal}>
                 Cancel
               </button>
               <button className="UD" onClick={handleUpdateUser}>
                 Update
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+
+      {showLogoutModal && (
+        <div className="modal">
+          <div className="modal-content">
+            <span
+              style={{
+                fontFamily: "Roboto",
+                fontStyle: "normal",
+                fontWeight: 500,
+                fontSize: "16px",
+                lineHeight: "14px",
+                color: "#001858",
+              }}
+            >
+              Do you want to log out?
+            </span>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-evenly",
+                marginTop: "1rem",
+              }}
+            >
+              <button
+                className="CLL"
+                style={{ marginRight: "2rem" }}
+                onClick={cancelLogout}
+              >
+                Cancel
+              </button>
+              <button className="LG" onClick={confirmLogout}>
+                Log Out
               </button>
             </div>
           </div>
